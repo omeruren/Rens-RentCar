69ec8561c056af0251f2262249c8abe6c3c614a9
omeruren <omer.uren19@gmail.com>
2026-02-13 19:36:56 +0300

    Created Domain Layer
    Created Base abstract entity. Created SetStatus and Delete methods.

---
+++ Rens-RentCar.Server/src/Rens-RentCar.Domain/Abstraction/BaseEntity.cs
@@ -0,0 +1,31 @@
+﻿namespace Rens_RentCar.Domain.Abstraction;
+
+public abstract class BaseEntity
+{
+    protected BaseEntity()
+    {
+        Id = new IdentityId(Guid.CreateVersion7());
+        IsActive = true;
+    }
+    public IdentityId Id { get; private set; }
+    public DateTimeOffset CreatedAt { get; private set; }
+    public IdentityId CreatedBy { get; private set; } = default!;
+
+    public IdentityId? UpdatedBy { get; private set; }
+    public DateTimeOffset? UpdatedAt { get; private set; }
+
+    public bool IsDeleted { get; private set; }
+    public IdentityId? DeletedBy { get; private set; }
+    public DateTimeOffset? DeletedAt { get; private set; }
+    public bool IsActive { get; private set; }
+
+
+    public void SetStatus(bool isActive) => IsActive = isActive;
+    public void Delete() => IsDeleted = true;
+
+}
+public sealed record IdentityId(Guid Value)
+{
+    public static implicit operator Guid(IdentityId id) => id.Value;
+    public static implicit operator string(IdentityId id) => id.Value.ToString();
+}
