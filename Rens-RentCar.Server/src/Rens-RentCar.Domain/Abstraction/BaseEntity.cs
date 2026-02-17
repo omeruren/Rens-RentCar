2155f7543cf314de90cdfa552e2aa7ed2e917a9c
omeruren <omer.uren19@gmail.com>
2026-02-17 22:35:33 +0300

    Created BranchGetQuery file.
    Added CreatedFullName and UpdatedFullName computed properties to BaseEntity.

--- Rens-RentCar.Server/src/Rens-RentCar.Domain/Abstraction/BaseEntity.cs
+++ Rens-RentCar.Server/src/Rens-RentCar.Domain/Abstraction/BaseEntity.cs
@@ -1,4 +1,8 @@
-﻿namespace Rens_RentCar.Domain.Abstraction;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Rens_RentCar.Domain.Users;
+
+namespace Rens_RentCar.Domain.Abstraction;
 
 public abstract class BaseEntity
 {
@@ -11,6 +15,10 @@ public abstract class BaseEntity
     public DateTimeOffset CreatedAt { get; private set; }
     public IdentityId CreatedBy { get; private set; } = default!;
 
+    public string CreatedFullName => GetCreatedFullName();
+    public string? UpdatedFullName => GetUpdatedFullName();
+
+
     public IdentityId? UpdatedBy { get; private set; }
     public DateTimeOffset? UpdatedAt { get; private set; }
 
@@ -22,6 +30,47 @@ public abstract class BaseEntity
 
     public void SetStatus(bool isActive) => IsActive = isActive;
     public void Delete() => IsDeleted = true;
+    private string GetCreatedFullName()
+    {
+        HttpContextAccessor httpContextAccessor = new();
+        var httpContext = httpContextAccessor.HttpContext;
+
+        if (httpContext is null)
+            return string.Empty;
+        using var scope = httpContext.RequestServices.CreateScope();
+        var userRepository = scope.ServiceProvider.GetService<IUserRepository>()!;
+
+        if (userRepository is null) return string.Empty;
+
+        var fullName = userRepository.FirstOrDefault(x => x.Id == CreatedBy).FullName;
+
+        if (fullName == null) return string.Empty;
+
+        return fullName.Value;
+    }
+    private string? GetUpdatedFullName()
+    {
+        if (UpdatedBy is not null)
+        {
+
+            HttpContextAccessor httpContextAccessor = new();
+            var httpContext = httpContextAccessor.HttpContext;
+
+            if (httpContext is null)
+                return string.Empty;
+            using var scope = httpContext.RequestServices.CreateScope();
+            var userRepository = scope.ServiceProvider.GetService<IUserRepository>()!;
+
+            if (userRepository is null) return string.Empty;
+
+            var fullName = userRepository.FirstOrDefault(x => x.Id == UpdatedBy).FullName;
+
+            if (fullName == null) return string.Empty;
+
+            return fullName.Value;
+        }
+        return null;
+    }
 
 }
 public sealed record IdentityId(Guid Value)
---
+++ Rens-RentCar.Server/src/Rens-RentCar.Server.Application/Branches/BranchGetQuery.cs
@@ -0,0 +1,19 @@
+﻿using Rens_RentCar.Domain.Branches;
+using TS.MediatR;
+using TS.Result;
+
+namespace Rens_RentCar.Server.Application.Branches;
+
+public sealed record BranchGetQuery(Guid Id) : IRequest<Result<Branch>>;
+
+internal sealed class BranchGetQueryHandler(IBranchRepository _branchRepository) : IRequestHandler<BranchGetQuery, Result<Branch>>
+{
+    public async Task<Result<Branch>> Handle(BranchGetQuery request, CancellationToken cancellationToken)
+    {
+        var branch = await _branchRepository.FirstOrDefaultAsync(x => x.Id == request.Id, cancellationToken);
+        if (branch is null)
+            return Result<Branch>.Failure("Branch not found.");
+
+        return branch;
+    }
+}
