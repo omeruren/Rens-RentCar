d478c7350ebd5bf0ecb247eedbe49b87cbd452ba
omeruren <omer.uren19@gmail.com>
2026-02-20 10:36:01 +0300

    DeleteRoleCommand file created.

---
+++ Rens-RentCar.Server/src/Rens-RentCar.Server.Application/Roles/RoleDeleteCommand.cs
@@ -0,0 +1,24 @@
+﻿using GenericRepository;
+using Rens_RentCar.Domain.Roles;
+using TS.MediatR;
+using TS.Result;
+
+namespace Rens_RentCar.Server.Application.Roles;
+
+public sealed record RoleDeleteCommand(Guid Id) : IRequest<Result<string>>;
+
+internal sealed class RoleDeleteCommandHandler(IRoleRepository _roleRepository, IUnitOfWork _unitOfWork) : IRequestHandler<RoleDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(RoleDeleteCommand request, CancellationToken cancellationToken)
+    {
+        Role? role = await _roleRepository.FirstOrDefaultAsync(r => r.Id == request.Id, cancellationToken);
+
+        if (role is null)
+            return Result<string>.Failure("Role not found.");
+
+        role.Delete();
+        _roleRepository.Update(role);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+        return "Role Deleted successfully.";
+    }
+}
