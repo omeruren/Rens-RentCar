cd11afdc46c0a6ce1130f0e5e99b15105a3f1779
omeruren <omer.uren19@gmail.com>
2026-02-20 10:50:55 +0300

    RoleGetQuery file created.

---
+++ Rens-RentCar.Server/src/Rens-RentCar.Domain/Roles/RoleDto.cs
@@ -0,0 +1,31 @@
+﻿using Rens_RentCar.Domain.Abstraction;
+
+namespace Rens_RentCar.Domain.Roles;
+
+public sealed class RoleDto : BaseEntityDto
+{
+    public string Name { get; set; } = default!;
+}
+public static class RoleExtensions
+{
+    public static IQueryable<RoleDto> MapTo(this IQueryable<EntityWithAuditDto<Role>> entities)
+    {
+        var res = entities.Select(s => new RoleDto
+        {
+            Id = s.Entity.Id,
+            Name = s.Entity.Name.Value,
+
+            IsActive = s.Entity.IsActive,
+            CreatedAt = s.Entity.CreatedAt,
+            CreatedBy = s.Entity.CreatedBy,
+            CreatedFullName = s.CreatedUser.FullName.Value,
+
+            UpdatedAt = s.Entity.UpdatedAt,
+            UpdatedBy = s.Entity.UpdatedBy != null ? s.Entity.UpdatedBy.Value : null,
+            UpdatedFullName = s.UpdatedUser != null ? s.UpdatedUser.FullName.Value : null
+        })
+            .AsQueryable();
+
+        return res;
+    }
+}
---
+++ Rens-RentCar.Server/src/Rens-RentCar.Server.Application/Roles/RoleGetQuery.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Rens_RentCar.Domain.Roles;
+using TS.MediatR;
+using TS.Result;
+
+namespace Rens_RentCar.Server.Application.Roles;
+
+public sealed record RoleGetQuery(Guid Id) : IRequest<Result<RoleDto>>;
+
+internal sealed class RoleGetQueryHandler(IRoleRepository _roleRepository) : IRequestHandler<RoleGetQuery, Result<RoleDto>>
+{
+    public async Task<Result<RoleDto>> Handle(RoleGetQuery request, CancellationToken cancellationToken)
+    {
+        var res = await _roleRepository
+            .GetAllWithAuditInfos()
+            .MapTo()
+            .Where(r => r.Id == request.Id)
+            .FirstOrDefaultAsync(cancellationToken);
+
+        if (res is null)
+            return Result<RoleDto>.Failure("Role nor found.");
+
+        return res;
+    }
+}
