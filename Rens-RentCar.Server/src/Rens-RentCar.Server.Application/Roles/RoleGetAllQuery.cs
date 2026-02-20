ddabb6f653514325d0412518369b24d027431da9
omeruren <omer.uren19@gmail.com>
2026-02-20 10:58:10 +0300

    RoleGetAllQuery file created.

---
+++ Rens-RentCar.Server/src/Rens-RentCar.Server.Application/Roles/RoleGetAllQuery.cs
@@ -0,0 +1,19 @@
+﻿using Rens_RentCar.Domain.Roles;
+using TS.MediatR;
+
+namespace Rens_RentCar.Server.Application.Roles;
+
+public sealed record RoleGetAllQuery : IRequest<IQueryable<RoleDto>>;
+
+internal sealed class RoleGetAllQueryHandler(IRoleRepository _roleRepository) : IRequestHandler<RoleGetAllQuery, IQueryable<RoleDto>>
+{
+    public Task<IQueryable<RoleDto>> Handle(RoleGetAllQuery request, CancellationToken cancellationToken)
+    {
+        var res = _roleRepository
+            .GetAllWithAuditInfos()
+            .MapTo();
+
+        return Task.FromResult(res);
+
+    }
+}
