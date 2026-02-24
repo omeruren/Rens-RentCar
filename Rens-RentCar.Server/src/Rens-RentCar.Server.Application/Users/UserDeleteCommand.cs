e88cf1b12da4cf86d2a5fce18c84805953061e72
omeruren <omer.uren19@gmail.com>
2026-02-24 15:32:33 +0300

    Created UserDeleteCommand file.

---
+++ Rens-RentCar.Server/src/Rens-RentCar.Server.Application/Users/UserDeleteCommand.cs
@@ -0,0 +1,26 @@
+﻿using GenericRepository;
+using Rens_RentCar.Domain.Users;
+using TS.MediatR;
+using TS.Result;
+
+namespace Rens_RentCar.Server.Application.Users;
+
+public sealed record UserDeleteCommand(Guid Id) : IRequest<Result<string>>;
+
+
+internal sealed class UserDeleteCommandHandler(IUserRepository _userRepository, IUnitOfWork _unitOfWork) : IRequestHandler<UserDeleteCommand, Result<string>>
+{
+    public async Task<Result<string>> Handle(UserDeleteCommand request, CancellationToken cancellationToken)
+    {
+        var user = await _userRepository.FirstOrDefaultAsync(u => u.Id == request.Id, cancellationToken);
+
+        if (user is null)
+            return Result<string>.Failure("User not found.");
+
+        user.Delete();
+        _userRepository.Update(user);
+        await _unitOfWork.SaveChangesAsync(cancellationToken);
+
+        return "User deleted successfully.";
+    }
+}
