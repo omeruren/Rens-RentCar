e59b0afebb4d5c9ac5464aeb3caacf62a1805f22
omeruren <omer.uren19@gmail.com>
2026-02-24 16:02:15 +0300

    Created UserGetAllQuey file.

---
+++ Rens-RentCar.Server/src/Rens-RentCar.Server.Application/Users/UserGetAllQuery.cs
@@ -0,0 +1,21 @@
+﻿using Rens_RentCar.Domain.Branches;
+using Rens_RentCar.Domain.Roles;
+using Rens_RentCar.Domain.Users;
+using TS.MediatR;
+
+namespace Rens_RentCar.Server.Application.Users;
+
+public sealed record UserGetAllQuery : IRequest<IQueryable<UserDto>>;
+
+internal sealed class UserGetAllQueryHandler(
+    IUserRepository _userRepository,
+    IRoleRepository _roleRepository,
+    IBranchRepository _branchRepository) : IRequestHandler<UserGetAllQuery, IQueryable<UserDto>>
+{
+    public Task<IQueryable<UserDto>> Handle(UserGetAllQuery request, CancellationToken cancellationToken)
+    {
+        var res = _userRepository.GetAllWithAuditInfos().MapTo(_roleRepository.GetAll(), _branchRepository.GetAll());
+
+        return Task.FromResult(res);
+    }
+}
