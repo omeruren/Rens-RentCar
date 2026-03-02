0f503a8fc16f52661a0446f7b7b0e17051069ac3
omeruren <omer.uren19@gmail.com>
2026-03-03 00:02:11 +0300

    migrated model to database

---
+++ Rens-RentCar.Server/src/Rens-RentCar.Server.Infrastructure/Migrations/20260302210126_mig_added_Customer_model.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Rens_RentCar.Server.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class mig_added_Customer_model : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Customers",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    NationalId_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    FirstName_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    LastName_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    FullName_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    BirthDate_Value = table.Column<DateOnly>(type: "date", nullable: false),
+                    PhoneNumber_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    Email_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    DrivingLicenceIssuanceDate_Value = table.Column<DateOnly>(type: "date", nullable: false),
+                    FullAddress_Value = table.Column<string>(type: "nvarchar(MAX)", nullable: false),
+                    CreatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: false),
+                    CreatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: false),
+                    UpdatedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    UpdatedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    IsDeleted = table.Column<bool>(type: "bit", nullable: false),
+                    DeletedBy = table.Column<Guid>(type: "uniqueidentifier", nullable: true),
+                    DeletedAt = table.Column<DateTimeOffset>(type: "datetimeoffset", nullable: true),
+                    IsActive = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Customers", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Customers");
+        }
+    }
+}
